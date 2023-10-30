84c733fb9d6bb92c4dc4c8aa97a4e55a74ca8db4
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-10-30 18:58:02 +0000

    Filter with a non-lambda function

--- filter_example.py
+++ filter_example.py
@@ -5,9 +5,15 @@ Get an array of all the elements that are even
 '''
 
 
+def is_even(num):
+    return num % 2 == 0
+
+
 if __name__ == '__main__':
-    new_array = list(filter(lambda num: num % 2 == 0, start_array))
+    new_array = list(filter(lambda element: element % 2 == 0, start_array))
+    new_array_again = list(filter(is_even, start_array))
     print(new_array)
+    print(new_array_again)
     '''
     The naive way:
     new_array = []
