a159d93e125e1759ba8539585f5b10505e2d08f5
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-10-30 19:14:11 +0000

    Reduce example

--- reduce_example.py
+++ reduce_example.py
@@ -1,13 +1,25 @@
+from functools import reduce
+
 start_array = ['Hello', 'my', 'name', 'is', 'Freddie']
 
 '''
 Given an array of strings, I want a new string which is all the words repeated (i.e. printed twice)
 '''
 
+
+def reduction(current, next):
+    return current + next + next
+
+
 if __name__ == '__main__':
+    result = reduce(reduction, start_array, 'Hello')
+    print(result)
+    '''
+    This is the naive way:
     result = ''
     for word in start_array:
         result += word
         result += word
     print(result)
+    '''
 
